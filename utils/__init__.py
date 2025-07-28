6ad73a6e91c4b425fd18e2e0498ca94828a0cc43
Torward1024 <almax1024@gmail.com>
2025-07-28 12:26:09 +0300

    28.07.2025

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,5 @@
 # msb/utils/__init__.py
-from .logging_setup import setup_logging, update_logging_level, update_logging_clear
+from .logging_setup import logger, setup_logging, update_logging_level, update_logging_clear
 from .validation import check_non_empty_string
 
-__all__ = ["setup_logging", "update_logging_level", "update_logging_clear", "check_non_empty_string"]
+__all__ = ["logger", "setup_logging", "update_logging_level", "update_logging_clear", "check_non_empty_string"]
